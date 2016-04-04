b2468eb7175b00e0e7e4b2689f78ce2cc6240175
fszlin <fszlin@gmail.com>
2016-04-04 15:00:53 -0400

    Add webscoket transport.

--- src/Microsoft.AspNet.SignalR.Client45/Transports/WebSocketTransport.cs
+++ src/Microsoft.AspNet.SignalR.Client45/Transports/WebSocketTransport.cs
@@ -6,18 +6,28 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNet.SignalR.Client.Http;
 using Microsoft.AspNet.SignalR.Client.Infrastructure;
+#if !NET35
 using Microsoft.AspNet.SignalR.Client.Transports.WebSockets;
+#else
+using WebSocketSharp;
+#endif
 
 namespace Microsoft.AspNet.SignalR.Client.Transports
 {
     public class WebSocketTransport : ClientTransportBase
     {
+#if !NET35
         private readonly ClientWebSocketHandler _webSocketHandler;
+#endif
         private CancellationToken _disconnectToken;
         private IConnection _connection;
         private string _connectionData;
         private CancellationTokenSource _webSocketTokenSource;
+#if !NET35
         private ClientWebSocket _webSocket;
+#else
+        private WebSocket _webSocket;
+#endif
         private int _disposed;
 
         public WebSocketTransport()
@@ -30,20 +40,24 @@ namespace Microsoft.AspNet.SignalR.Client.Transports
         {
             _disconnectToken = CancellationToken.None;
             ReconnectDelay = TimeSpan.FromSeconds(2);
+#if !NET35
             _webSocketHandler = new ClientWebSocketHandler(this);
+#endif
         }
 
+#if !NET35
         // intended for testing
         internal WebSocketTransport(ClientWebSocketHandler webSocketHandler)
             : this()
         {
             _webSocketHandler = webSocketHandler;
         }
+#endif
 
-        /// <summary>
-        /// The time to wait after a connection drops to try reconnecting.
-        /// </summary>
-        public TimeSpan ReconnectDelay { get; set; }
+    /// <summary>
+    /// The time to wait after a connection drops to try reconnecting.
+    /// </summary>
+    public TimeSpan ReconnectDelay { get; set; }
 
         /// <summary>
         /// Indicates whether or not the transport supports keep alive
@@ -80,14 +94,19 @@ namespace Microsoft.AspNet.SignalR.Client.Transports
             return PerformConnect(UrlBuilder.BuildConnect(_connection, Name, _connectionData));
         }
 
+#if !NET35
         private async Task PerformConnect(string url)
+#else
+        private Task PerformConnect(string url)
+#endif
         {
             var uri = UrlBuilder.ConvertToWebSocketUri(url);
 
             _connection.Trace(TraceLevels.Events, "WS Connecting to: {0}", uri);
- 
+
             // TODO: Revisit thread safety of this assignment
             _webSocketTokenSource = new CancellationTokenSource();
+#if !NET35
             _webSocket = new ClientWebSocket();
 
             _connection.PrepareRequest(new WebSocketWrapperRequest(_webSocket, _connection));
@@ -97,6 +116,17 @@ namespace Microsoft.AspNet.SignalR.Client.Transports
 
             await _webSocket.ConnectAsync(uri, token);
             await _webSocketHandler.ProcessWebSocketRequestAsync(_webSocket, token);
+#else
+            _webSocket = new WebSocket(uri.AbsoluteUri);
+
+            _webSocket.OnOpen += (sender, e) => this.OnOpen();
+            _webSocket.OnClose += (sender, e) => this.OnClose();
+            _webSocket.OnMessage += (sender, e) => this.OnMessage(e.Data);
+            _webSocket.OnError += (sender, e) => this.OnError(e.Exception);
+
+            _webSocket.Connect();
+            return TaskAsyncHelper.Empty;
+#endif
         }
 
         protected override void OnStartFailed()
@@ -112,8 +142,12 @@ namespace Microsoft.AspNet.SignalR.Client.Transports
                 throw new ArgumentNullException("connection");
             }
 
+#if !NET35
             // If we don't throw here when the WebSocket isn't open, WebSocketHander.SendAsync will noop.
             if (_webSocketHandler.WebSocket.State != WebSocketState.Open)
+#else
+            if (_webSocket.ReadyState != WebSocketState.Open)
+#endif
             {
                 // Make this a faulted task and trigger the OnError even to maintain consistency with the HttpBasedTransports
                 var ex = new InvalidOperationException(Resources.Error_DataCannotBeSentDuringWebSocketReconnect);
@@ -121,7 +155,12 @@ namespace Microsoft.AspNet.SignalR.Client.Transports
                 return TaskAsyncHelper.FromError(ex);
             }
 
+#if !NET35
             return _webSocketHandler.SendAsync(data);
+#else
+            _webSocket.Send(data);
+            return TaskAsyncHelper.Empty;
+#endif
         }
 
         // virtual for testing
@@ -161,32 +200,58 @@ namespace Microsoft.AspNet.SignalR.Client.Transports
         }
 
         // fire and forget
+#if !NET35
         private async void DoReconnect()
+#else
+        private void DoReconnect()
+#endif
         {
             var reconnectUrl = UrlBuilder.BuildReconnect(_connection, Name, _connectionData);
 
+#if !NET35
             while (TransportHelper.VerifyLastActive(_connection) && _connection.EnsureReconnecting())
+#else
+            if (TransportHelper.VerifyLastActive(_connection) && _connection.EnsureReconnecting())
+#endif
             {
                 try
                 {
+#if !NET35
                     await PerformConnect(reconnectUrl);
                     break;
+#else
+                    PerformConnect(reconnectUrl);
+                    return;
+#endif
                 }
                 catch (OperationCanceledException)
                 {
+#if !NET35
                     break;
+#else
+                    return;
+#endif
                 }
                 catch (Exception ex)
                 {
                     if (ExceptionHelper.IsRequestAborted(ex))
                     {
+#if !NET35
                         break;
+#else
+                        return;
+#endif
                     }
 
                     _connection.OnError(ex);
                 }
 
+#if !NET35
                 await Task.Delay(ReconnectDelay);
+#else
+                TaskAsyncHelper.Delay(ReconnectDelay)
+                    .Then(() => DoReconnect());
+#endif
             }
         }
 
@@ -224,7 +289,11 @@ namespace Microsoft.AspNet.SignalR.Client.Transports
 
                 if (_webSocket != null)
                 {
+#if !NET35
                     _webSocket.Dispose();
+#else
+                    using (_webSocket) { }
+#endif
                 }
 
                 if (_webSocketTokenSource != null)
